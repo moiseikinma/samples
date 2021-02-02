b83fb6b6f38eb92b9b6b59e79c2c7121da41fd20
Mikhail <moiseikin.ma@gmail.com>
2021-02-03 00:11:37 +0300

    ssl boost beast

---
+++ boostbeastssl/main.cpp
@@ -0,0 +1,202 @@
+﻿#include <windows.h>
+#include <wincrypt.h>
+
+#include <fstream>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <string_view>
+
+#include <boost/asio.hpp>
+#include <boost/asio/ssl.hpp>
+#include <boost/beast.hpp>
+#include <boost/beast/ssl.hpp>
+
+#include <boost/asio/buffer.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/beast/websocket.hpp>
+
+#include <boost/asio.hpp>
+#include <boost/beast.hpp>
+
+
+
+void add_windows_root_certs(boost::asio::ssl::context& ctx);
+void load_root_certificates(boost::asio::ssl::context& ctx, boost::system::error_code& ec);
+
+int main() {
+
+    auto const host = "cars.skoda-avto.ru";
+
+
+    boost::system::error_code ec;
+    boost::asio::io_context ioc;
+    boost::asio::ssl::context ctx{ boost::asio::ssl::context::tlsv12_client };
+
+
+    // This holds the root certificate used for verification
+    //load_root_certificates(ctx, ec);
+    add_windows_root_certs(ctx);
+
+    // Verify the remote server's certificate
+    ctx.set_verify_mode(boost::asio::ssl::verify_peer);
+
+    boost::asio::ip::tcp::resolver resolver{ ioc };
+    boost::beast::ssl_stream<boost::beast::tcp_stream> stream(ioc, ctx);
+
+    // Set SNI Hostname (many hosts need this to handshake successfully)
+    if (!SSL_set_tlsext_host_name(stream.native_handle(), host)) {
+        boost::beast::error_code ec{ static_cast<int>(::ERR_get_error()),
+                                      boost::asio::error::get_ssl_category() };
+        throw boost::beast::system_error{ ec };
+    }
+
+
+    //boost::asio::ip::tcp::socket socket{ ioc };
+    auto const results = resolver.resolve("cars.skoda-avto.ru", "443");
+
+    //boost::asio::connect(socket, result.begin(), result.end());
+    // Make the connection on the IP address we get from a lookup
+    boost::beast::get_lowest_layer(stream).connect(results);
+
+    // Perform the SSL handshake
+    stream.handshake(boost::asio::ssl::stream_base::client);
+
+
+    boost::beast::http::request<boost::beast::http::string_body> req{
+        boost::beast::http::verb::get, "/", 11
+    };
+    req.set(boost::beast::http::field::host, host);
+    req.set(boost::beast::http::field::user_agent, BOOST_BEAST_VERSION_STRING);
+
+    boost::beast::http::write(stream, req);
+
+    // This buffer is used for reading and must be persisted
+    boost::beast::flat_buffer buffer;
+
+    // Declare a container to hold the response
+    boost::beast::http::response<boost::beast::http::dynamic_body> res;
+
+    // Receive the HTTP response
+    boost::beast::http::read(stream, buffer, res);
+
+    // Write the message to standard out
+    std::cout << res << std::endl;
+
+    std::ofstream ofs{ "https-resp.txt", std::ios::binary | std::ios::out };
+    ofs << res;
+    ofs.close();
+
+
+    // Gracefully close the socket
+    //boost::system::error_code ec;
+    stream.shutdown(ec);
+
+    return 0;
+}
+
+
+void add_windows_root_certs(boost::asio::ssl::context& ctx) {
+    HCERTSTORE hStore = CertOpenSystemStore(0, L"ROOT");
+    if (hStore == NULL) {
+        return;
+    }
+
+    X509_STORE* store = X509_STORE_new();
+    PCCERT_CONTEXT pContext = NULL;
+    while ((pContext = CertEnumCertificatesInStore(hStore, pContext)) != NULL) {
+        X509* x509 = d2i_X509(
+            NULL, (const unsigned char**)&pContext->pbCertEncoded, pContext->cbCertEncoded);
+        if (x509 != NULL) {
+            X509_STORE_add_cert(store, x509);
+            X509_free(x509);
+        }
+    }
+
+    CertFreeCertificateContext(pContext);
+    CertCloseStore(hStore, 0);
+
+    SSL_CTX_set_cert_store(ctx.native_handle(), store);
+}
+
+
+void load_root_certificates(boost::asio::ssl::context& ctx, boost::system::error_code& ec) {
+    std::string const cert =
+        /*  This is the DigiCert Global Root CA
+
+            CN = DigiCert High Assurance EV Root CA
+            OU = www.digicert.com
+            O = DigiCert Inc
+            C = US
+
+            Valid to: 10 November 2031
+
+            Serial #:
+            08:3B:E0:56:90:42:46:B1:A1:75:6A:C9:59:91:C7:4A
+
+            SHA1 Fingerprint:
+            A8:98:5D:3A:65:E5:E5:C4:B2:D7:D6:6D:40:C6:DD:2F:B1:9C:54:36
+
+            SHA256 Fingerprint:
+            43:48:A0:E9:44:4C:78:CB:26:5E:05:8D:5E:89:44:B4:D8:4F:96:62:BD:26:DB:25:7F:89:34:A4:43:C7:01:61
+        */
+        "-----BEGIN CERTIFICATE-----\n"
+        "MIIDrzCCApegAwIBAgIQCDvgVpBCRrGhdWrJWZHHSjANBgkqhkiG9w0BAQUFADBh\n"
+        "MQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3\n"
+        "d3cuZGlnaWNlcnQuY29tMSAwHgYDVQQDExdEaWdpQ2VydCBHbG9iYWwgUm9vdCBD\n"
+        "QTAeFw0wNjExMTAwMDAwMDBaFw0zMTExMTAwMDAwMDBaMGExCzAJBgNVBAYTAlVT\n"
+        "MRUwEwYDVQQKEwxEaWdpQ2VydCBJbmMxGTAXBgNVBAsTEHd3dy5kaWdpY2VydC5j\n"
+        "b20xIDAeBgNVBAMTF0RpZ2lDZXJ0IEdsb2JhbCBSb290IENBMIIBIjANBgkqhkiG\n"
+        "9w0BAQEFAAOCAQ8AMIIBCgKCAQEA4jvhEXLeqKTTo1eqUKKPC3eQyaKl7hLOllsB\n"
+        "CSDMAZOnTjC3U/dDxGkAV53ijSLdhwZAAIEJzs4bg7/fzTtxRuLWZscFs3YnFo97\n"
+        "nh6Vfe63SKMI2tavegw5BmV/Sl0fvBf4q77uKNd0f3p4mVmFaG5cIzJLv07A6Fpt\n"
+        "43C/dxC//AH2hdmoRBBYMql1GNXRor5H4idq9Joz+EkIYIvUX7Q6hL+hqkpMfT7P\n"
+        "T19sdl6gSzeRntwi5m3OFBqOasv+zbMUZBfHWymeMr/y7vrTC0LUq7dBMtoM1O/4\n"
+        "gdW7jVg/tRvoSSiicNoxBN33shbyTApOB6jtSj1etX+jkMOvJwIDAQABo2MwYTAO\n"
+        "BgNVHQ8BAf8EBAMCAYYwDwYDVR0TAQH/BAUwAwEB/zAdBgNVHQ4EFgQUA95QNVbR\n"
+        "TLtm8KPiGxvDl7I90VUwHwYDVR0jBBgwFoAUA95QNVbRTLtm8KPiGxvDl7I90VUw\n"
+        "DQYJKoZIhvcNAQEFBQADggEBAMucN6pIExIK+t1EnE9SsPTfrgT1eXkIoyQY/Esr\n"
+        "hMAtudXH/vTBH1jLuG2cenTnmCmrEbXjcKChzUyImZOMkXDiqw8cvpOp/2PV5Adg\n"
+        "06O/nVsJ8dWO41P0jmP6P6fbtGbfYmbW0W5BjfIttep3Sp+dWOIrWcBAI+0tKIJF\n"
+        "PnlUkiaY4IBIqDfv8NZ5YBberOgOzW6sRBc4L0na4UU+Krk2U886UAb3LujEV0ls\n"
+        "YSEY1QSteDwsOoBrp+uvFRTp2InBuThs4pFsiv9kuXclVzDAGySj4dzp30d8tbQk\n"
+        "CAUw7C29C79Fv1C5qfPrmAESrciIxpg0X40KPMbp1ZWVbd4=\n"
+        "-----END CERTIFICATE-----\n"
+
+        /*  This is the GeoTrust root certificate.
+
+            CN = GeoTrust Global CA
+            O = GeoTrust Inc.
+            C = US
+            Valid to: Friday, ‎May ‎20, ‎2022 9:00:00 PM
+
+            Thumbprint(sha1):
+            ‎de 28 f4 a4 ff e5 b9 2f a3 c5 03 d1 a3 49 a7 f9 96 2a 82 12
+        */
+        "-----BEGIN CERTIFICATE-----\n"
+        "MIIDaDCCAlCgAwIBAgIJAO8vBu8i8exWMA0GCSqGSIb3DQEBCwUAMEkxCzAJBgNV\n"
+        "BAYTAlVTMQswCQYDVQQIDAJDQTEtMCsGA1UEBwwkTG9zIEFuZ2VsZXNPPUJlYXN0\n"
+        "Q049d3d3LmV4YW1wbGUuY29tMB4XDTE3MDUwMzE4MzkxMloXDTQ0MDkxODE4Mzkx\n"
+        "MlowSTELMAkGA1UEBhMCVVMxCzAJBgNVBAgMAkNBMS0wKwYDVQQHDCRMb3MgQW5n\n"
+        "ZWxlc089QmVhc3RDTj13d3cuZXhhbXBsZS5jb20wggEiMA0GCSqGSIb3DQEBAQUA\n"
+        "A4IBDwAwggEKAoIBAQDJ7BRKFO8fqmsEXw8v9YOVXyrQVsVbjSSGEs4Vzs4cJgcF\n"
+        "xqGitbnLIrOgiJpRAPLy5MNcAXE1strVGfdEf7xMYSZ/4wOrxUyVw/Ltgsft8m7b\n"
+        "Fu8TsCzO6XrxpnVtWk506YZ7ToTa5UjHfBi2+pWTxbpN12UhiZNUcrRsqTFW+6fO\n"
+        "9d7xm5wlaZG8cMdg0cO1bhkz45JSl3wWKIES7t3EfKePZbNlQ5hPy7Pd5JTmdGBp\n"
+        "yY8anC8u4LPbmgW0/U31PH0rRVfGcBbZsAoQw5Tc5dnb6N2GEIbq3ehSfdDHGnrv\n"
+        "enu2tOK9Qx6GEzXh3sekZkxcgh+NlIxCNxu//Dk9AgMBAAGjUzBRMB0GA1UdDgQW\n"
+        "BBTZh0N9Ne1OD7GBGJYz4PNESHuXezAfBgNVHSMEGDAWgBTZh0N9Ne1OD7GBGJYz\n"
+        "4PNESHuXezAPBgNVHRMBAf8EBTADAQH/MA0GCSqGSIb3DQEBCwUAA4IBAQCmTJVT\n"
+        "LH5Cru1vXtzb3N9dyolcVH82xFVwPewArchgq+CEkajOU9bnzCqvhM4CryBb4cUs\n"
+        "gqXWp85hAh55uBOqXb2yyESEleMCJEiVTwm/m26FdONvEGptsiCmF5Gxi0YRtn8N\n"
+        "V+KhrQaAyLrLdPYI7TrwAOisq2I1cD0mt+xgwuv/654Rl3IhOMx+fKWKJ9qLAiaE\n"
+        "fQyshjlPP9mYVxWOxqctUdQ8UnsUKKGEUcVrA08i1OAnVKlPFjKBvk+r7jpsTPcr\n"
+        "9pWXTO9JrYMML7d+XRSZA1n3856OqZDX4403+9FnXCvfcLZLLKTBvwwFgEFGpzjK\n"
+        "UEVbkhd5qstF6qWK\n"
+        "-----END CERTIFICATE-----\n";
+    ;
+
+    ctx.add_certificate_authority(boost::asio::buffer(cert.data(), cert.size()), ec);
+    if (ec)
+        return;
+}
